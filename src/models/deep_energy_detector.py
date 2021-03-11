3439eb595101131c77118e4ff6be7196fea068f9
georgegrosu1 <george.grosu.floring@gmail.com>
2021-03-11 12:46:16 +0200

    Minor modifications to DL model for energy detection

--- src/models/deep_energy_detector.py
+++ src/models/deep_energy_detector.py
@@ -1,10 +1,11 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import optimizers
+from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
 from tensorflow.keras.layers import Input, Conv1D, LSTM, Dense, Dropout, MaxPooling1D
 
 
-def build_model(input_dim, output_dim, num_features, metrics: list, loss='sigmoid'):
+def build_model(input_dim, output_dim, num_features, metrics: list, loss=binary_crossentropy):
     lr_schedule = ExponentialDecay(initial_learning_rate=1e-5,
                                    decay_steps=10000,
                                    decay_rate=0.7)
@@ -12,6 +13,10 @@ def build_model(input_dim, output_dim, num_features, metrics: list, loss='sigmoi
 
     model = Sequential()
     model.add(Input(shape=(input_dim, num_features)))
+    # model.add(Conv1D(16, (50, )))
+    # model.add(MaxPooling1D())
+    # model.add(Conv1D(32, (50,)))
+    # model.add(MaxPooling1D())
     model.add(LSTM(units=64, return_state=True, unit_forget_bias=True))
     model.add(LSTM(units=64, return_state=False, unit_forget_bias=True))
     model.add(Dense(32, activation='swish'))
