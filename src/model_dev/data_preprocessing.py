c6790090908cb1f5bd4e1dba2553cb45de1c8f89
georgegrosu1 <george.grosu.floring@gmail.com>
2021-03-22 21:39:26 +0200

    Updated generator and model creation

--- src/model_dev/data_preprocessing.py
+++ src/model_dev/data_preprocessing.py
@@ -5,28 +5,32 @@ from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 
 
 class TimeSeriesFeeder:
-    def __init__(self, data_path: Path, target_features: list, window_dim: int, feed_batch: int):
+    def __init__(self, data_path: Path, target_features: list, window_dim: int, feed_batch: int, stride: int = 1):
         self.data_path = data_path
         self.target_features = target_features
         self.window_dim = window_dim
-        self.main_dataframe = self.get_data_from_path()
+        self.generator = self._init_generator()
         self.feed_batch = feed_batch
+        self.stride = stride
+
+    def _init_generator(self):
+        main_dataframe = self.get_data_from_path()
+        data = main_dataframe.to_numpy()
+        targets = main_dataframe.index.tolist()
+        generator = TimeseriesGenerator(data, targets, length=self.window_dim,
+                                        batch_size=self.feed_batch, stride=self.stride)
+
+        return generator
 
     def get_data_from_path(self):
         if 'csv' in self.data_path.suffix:
             return pd.read_csv(str(self.data_path))
         dfs = []
-        for file_p in self.data_path.glob('*.csv'):
+        all_csv_files = self.data_path.glob('*.csv')
+        for file_p in all_csv_files:
             df = pd.read_csv(str(file_p))
             dfs.append(df)
         return pd.concat(dfs, axis=0)
 
-    def feed_generator(self, batch_dim=None):
-        data = self.main_dataframe.to_numpy()
-        targets = self.main_dataframe.index.tolist()
-        if batch_dim is not None:
-            batch_s = batch_dim
-        else:
-            batch_s = self.feed_batch
-        generator = TimeseriesGenerator(data, targets, length=self.window_dim, batch_size=batch_s)
-        return generator
+    def feed_generator(self):
+        return self.generator
--- src/models/deep_energy_detector.py
+++ src/models/deep_energy_detector.py
@@ -2,17 +2,22 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras import optimizers
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
-from tensorflow.keras.layers import Input, Conv1D, LSTM, Dense, Dropout, MaxPooling1D
+from tensorflow.keras.layers import Input, Conv1D, LSTM, Dense, Dropout, MaxPooling1D, BatchNormalization
 
 
-def build_model(input_dim, output_dim, num_features, metrics: list, loss=binary_crossentropy):
+def build_model(input_dim, output_dim, num_features, metrics: list, loss=binary_crossentropy, optimizer=None):
     lr_schedule = ExponentialDecay(initial_learning_rate=1e-5,
                                    decay_steps=10000,
                                    decay_rate=0.7)
-    optimizer = optimizers.Adam(learning_rate=lr_schedule)
+    if optimizer is not None:
+        optimizer = optimizer
+    else:
+        optimizer = optimizers.Adam(learning_rate=lr_schedule)
+    metrics += [binary_crossentropy]
 
     model = Sequential()
     model.add(Input(shape=(input_dim, num_features)))
+    model.add(BatchNormalization())
     # model.add(Conv1D(16, (50, )))
     # model.add(MaxPooling1D())
     # model.add(Conv1D(32, (50,)))
