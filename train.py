47bf830b03c8574b6793a7d5db32858e82183813
georgegrosu1 <george.grosu.floring@gmail.com>
2021-03-26 21:22:55 +0200

    Improvements in model training -model is nou ConvLSTM -weights are saved for each epoch in corresponding subdir -metrics take now Recall and Precision funcs

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from pathlib import Path
 from tensorflow_addons.metrics import F1Score
-from tensorflow.keras.metrics import KLDivergence, PrecisionAtRecall, RecallAtPrecision
+from tensorflow.keras.metrics import Recall, Precision
 from tensorflow.keras.callbacks import ModelCheckpoint
 from src.models.deep_energy_detector import build_model
 from src.model_dev.data_preprocessing import TimeSeriesFeeder
@@ -31,7 +31,8 @@ def get_abs_path(relative_path):
 
 
 def get_saving_model_path(configs, model_name: str):
-    save_dir = get_abs_path(configs['save_path'])
+    save_dir = get_abs_path(configs['save_path']) / model_name
+    model_name = model_name + '_epoch{epoch:02d}_vloss{val_loss:.2f}.hdf5'
     return save_dir / model_name
 
 
@@ -57,7 +58,6 @@ def train_energy_detector(config_path, model_name, model=None):
     epochs = configs['train_cfg']['epochs']
     pos_thresh = configs['model_cfg']['positive_threshold']
 
-
     train_feeder = TimeSeriesFeeder(data_path=training_path,
                                     x_features=input_features,
                                     y_features=output_features,
@@ -71,7 +71,9 @@ def train_energy_detector(config_path, model_name, model=None):
 
     f1_score = F1Score(num_classes=2 ** num_outputs,
                        average="micro", threshold=pos_thresh)
-    m_metrics = ['accuracy', f1_score]
+    recall = Recall(thresholds=pos_thresh)
+    precision = Precision(thresholds=pos_thresh)
+    m_metrics = ['accuracy', f1_score, recall, precision]
 
     if model is None:
         num_inputs = len(input_features)
@@ -85,6 +87,7 @@ def train_energy_detector(config_path, model_name, model=None):
         save_weights_only=False,
         monitor=f1_score,
         mode='max',
+        save_freq='epoch',
         save_best_only=False)
 
     model.fit(train_feeder.feed_generator(), epochs=epochs,
@@ -98,7 +101,7 @@ def main():
 
     args_parser = argparse.ArgumentParser(description='Training script for NNs energy detection')
     args_parser.add_argument('--config_path', '-c', type=str, help='Path to model', default=r'/configs/train.json')
-    args_parser.add_argument('--model_name', '-n', type=str, help='Path to model', default=r'energy_detect_v1.hdf5')
+    args_parser.add_argument('--model_name', '-n', type=str, help='Path to model', default=r'energy_detect_v1')
     args = args_parser.parse_args()
 
     train_energy_detector(args.config_path, args.model_name)
