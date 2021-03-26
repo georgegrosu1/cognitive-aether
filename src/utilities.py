ce3af13284c4ec503f7584261ab5efb825be68dc
georgegrosu1 <george.grosu.floring@gmail.com>
2021-03-27 00:03:35 +0200

    Created utilities with functions used in multiple locations -scale removed from comm_channel

---
+++ src/utilities.py
@@ -0,0 +1,18 @@
+import numpy as np
+from collections import deque
+
+
+def slidewindow(seq, n=2):
+    it = iter(seq)
+    win = deque((next(it, None) for _ in range(n)), maxlen=n)
+    yield win
+    append = win.append
+    for e in it:
+        append(e)
+        yield win
+
+
+def scale(x, out_range=(0, 1), axis=None):
+    domain = np.min(x, axis), np.max(x, axis)
+    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
+    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2
