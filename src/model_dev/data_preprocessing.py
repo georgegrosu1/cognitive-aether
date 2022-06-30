6a9250180c458b8ef149d930bd789efe47a0639f
georgegrosu1 <george.grosu.florin@gmail.com>
2022-06-30 11:46:51 +0200

    Changed TimeSeriesFeeder -inherits tf.keras.preprocessing TimeSeriesGenerator -overrides base method to generate data -fixed bugs -transform are now applied not only for data taken from a path

--- src/model_dev/data_preprocessing.py
+++ src/model_dev/data_preprocessing.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from pathlib import Path
@@ -7,38 +8,82 @@ from sklearn.decomposition import PCA
 from src.utilities import scale
 
 
-class TimeSeriesFeeder:
-    def __init__(self, x_features: list, y_features: list,
-                 window_dim: int, feed_batch: int, stride: int = 1,
-                 min_max_scale: bool = True, pow_transform: bool = False,
-                 pca_transform=False, data_path: Path = None,
-                 use_dataframe: pd.DataFrame = None, shuffle=False):
+class TimeSeriesFeeder(TimeseriesGenerator):
+    def __init__(self,
+                 x_features: list,
+                 y_features: list,
+                 x_length: int,
+                 batch_size: int,
+                 start_index=0,
+                 end_index=None,
+                 y_length=None,
+                 sampling_rate=1,
+                 stride: int = 1,
+                 min_max_scale: bool = True,
+                 pow_transform: bool = False,
+                 pca_transform=False,
+                 data_path: Path = None,
+                 use_dataframe: pd.DataFrame = None,
+                 shuffle=False,
+                 reverse=False):
         self.data_path = data_path
         self.exogenous_features = x_features
         self.endogenous_features = y_features
-        self.window_dim = window_dim
-        self.feed_batch = feed_batch
-        self.stride = stride
+        if y_length is None:
+            self.y_length = x_length
+        else:
+            self.y_length = y_length
         self.min_max_scale = min_max_scale
         self.pow_transform = pow_transform
         self.pca_transform = pca_transform
         self.use_dataframe = use_dataframe
-        self.shuffle = shuffle
-        self.generator = self._init_generator()
 
-    def _init_generator(self):
+        data, targets = self._fetch_data_targets()
+        super().__init__(data,
+                         targets,
+                         x_length,
+                         sampling_rate,
+                         stride,
+                         start_index,
+                         end_index,
+                         shuffle,
+                         reverse,
+                         batch_size)
+
+    # Override parent class method for data generation
+    def __getitem__(self, index):
+        if self.shuffle:
+            rows = np.random.randint(
+                self.start_index, self.end_index + 1, size=self.batch_size)
+        else:
+            i = self.start_index + self.batch_size * self.stride * index
+            rows = np.arange(
+                i, min(i + self.batch_size * self.stride, self.end_index + 1),
+                self.stride)
+
+        samples = np.array([self.data[(row - self.length):row:self.sampling_rate] for row in rows])
+        targets = np.array([self.targets[row - self.y_length:row:self.sampling_rate] for row in rows])
+
+        if self.reverse:
+            return samples[:, ::-1, ...], targets
+        return samples, targets
+
+    def _fetch_data_targets(self):
         assert (self.use_dataframe is not None) | (self.data_path is not None), "Provide a dataframe or data path"
         if self.use_dataframe is None:
-            main_dataframe = self.get_data_from_path()
+            df = self.get_data_from_path()
         else:
-            main_dataframe = self.use_dataframe
-        x_data = main_dataframe.loc[:, self.exogenous_features].to_numpy()
-        y_data = main_dataframe.loc[:, self.endogenous_features].to_numpy()
-        generator = TimeseriesGenerator(x_data, y_data, length=self.window_dim,
-                                        batch_size=self.feed_batch,
-                                        stride=self.stride, shuffle=self.shuffle)
+            df = self.use_dataframe
+        if self.min_max_scale:
+            df.loc[:, self.exogenous_features] = df.loc[:, self.exogenous_features].apply(scale, axis=0)
+        if self.pow_transform:
+            df.loc[:, self.exogenous_features] = self.apply_powtransform(df.loc[:, self.exogenous_features])
+        if self.pca_transform:
+            df.loc[:, self.exogenous_features] = self.apply_pca_transform(df.loc[:, self.exogenous_features])
+        x_data = df.loc[:, self.exogenous_features].to_numpy()
+        y_data = df.loc[:, self.endogenous_features].to_numpy()
 
-        return generator
+        return x_data, y_data
 
     def apply_powtransform(self, df):
         transform_df = df.copy()
@@ -65,14 +110,5 @@ class TimeSeriesFeeder:
         for file_p in all_csv_files:
             df = pd.read_csv(str(file_p))
             df = df.loc[:, take_features]
-            if self.min_max_scale:
-                df = df.apply(scale, axis=0)
-            if self.pow_transform:
-                df = self.apply_powtransform(df)
-            if self.pca_transform:
-                df.loc[:, self.exogenous_features] = self.apply_pca_transform(df.loc[:, self.exogenous_features])
             dfs.append(df)
         return pd.concat(dfs, axis=0)
-
-    def feed_generator(self):
-        return self.generator
